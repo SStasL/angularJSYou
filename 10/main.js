101737e3ee76d60d55c0a9731f82c0a12e27c67d
SStasL <stas.shmulich@gmail.com>
2019-09-02 16:42:39 +0300

    up to lesson 13

--- 10/main.js
+++ 10/main.js
@@ -1,1 +1,26 @@
 var app = angular.module('app', []);
+
+app.directive("fooBar", function () {
+    var bookmarks = [
+        {
+            id: 1,
+            name: "AngularJS"
+        },
+        {
+            id: 2,
+            name: "EmberJS"
+        },
+        {
+            id: 3,
+            name: "ReactJS"
+        }
+    ]
+    return {
+        template: "<div ng-repeat='bookmark in myBookmarks'>{{bookmark.name}}</div>",
+        link: function (scope, elements, attrs) {
+            console.log("fooBar");
+            scope.name = "www";
+            scope.myBookmarks = bookmarks;
+        }
+    };
+});
---
+++ 11/main.js
@@ -0,0 +1,20 @@
+var app = angular.module('app', []);
+
+app.controller ('mainCtrl', function ($scope) {
+    $scope.name='Bob';
+});
+
+app.directive('fooBar', function () {
+    return {
+        restrist: "E",
+        transclude: true,
+        template: "This is my super directive",
+        link: function (scope, element, attrs, ctrl, transclude) {
+            console.log("This is my super directive");
+            transclude(scope, function(clone, scope) {
+                console.log('!',clone, scope);
+                element.append(clone);
+            });
+        },
+    }
+});
