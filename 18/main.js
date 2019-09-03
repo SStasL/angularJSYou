49664c285ac184c0ed0427250f13e5c19af00b3a
SStasL <stas.shmulich@gmail.com>
2019-09-03 12:52:08 +0300

    added by 18 lesson

--- 18/main.js
+++ 18/main.js
@@ -1,1 +1,24 @@
 var app = angular.module('app', []);
+
+app.controller('mainCtrl', function($http, $scope) {
+    $http.get('http://localhost:3001/books')
+        .then(function(result) {
+            console.log('success', result);
+            $scope.books = result.data;
+        }
+        ,function(result) {
+            console.log('error');
+        });
+    $scope.addBook = function(book) {
+        console.log(book);
+        $http.post('http://localhost:3001/books', book)
+            .then(function(result) {
+                console.log('book successefully saved');
+                $scope.books.push(book);
+                $scope.book = null;
+            }
+            ,function(result) {
+                console.log('error in book post');
+            });
+    }
+});
