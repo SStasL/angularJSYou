d33247422694e142626b036af741c761a5a8118d
SStasL <stas.shmulich@gmail.com>
2019-09-03 15:57:32 +0300

    added by 21 lesson

---
+++ 19/main.js
@@ -0,0 +1,44 @@
+var app = angular.module('app', ['ngMockE2E']);
+
+app.run(function($httpBackend) {
+    var books = [
+        {
+          name: 'AngularJS'
+        },
+        {
+          name: 'EmberJS'
+        },
+        {
+          name: 'ReactJS'
+        }
+      ];
+    $httpBackend.whenGET('http://localhost:3001/books').respond(200, books);
+    $httpBackend.whenPOST('http://localhost:3001/books').respond(function(method, url, data) {
+      var result = JSON.parse(data);
+      books.push(result);
+      return [200, result];
+    });
+});
+
+app.controller('mainCtrl', function($http, $scope) {
+    $http.get('http://localhost:3001/books')
+        .then(function(result) {
+            console.log('success', result);
+            $scope.books = result.data;
+        }
+        ,function(result) {
+            console.log('error');
+        });
+    $scope.addBook = function(book) {
+        console.log(book);
+        $http.post('http://localhost:3001/books', book)
+            .then(function(result) {
+                console.log('book successefully saved');
+                $scope.books.push(book);
+                $scope.book = null;
+            }
+            ,function(result) {
+                console.log('error in book post');
+            });
+    }
+});
